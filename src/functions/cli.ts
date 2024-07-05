7f344579636e3d8b6e6ba586a3b1ffd6de6f543f
Wilson Foo Yu Kang <wilson.foo@customautosys.com>
2024-07-05 23:30:43 +0800

    Added help text for command line options

--- src/functions/cli.ts
+++ src/functions/cli.ts
@@ -1,4 +1,15 @@
+import {Command} from 'commander';
+import package_json from '../../package.json';
+
 export function cli(){
+	const program=new Command();
+	program
+		.version(package_json.version)
+		.name('legaldac')
+		.option('-d, --debug','enables verbose logging',false)
+		.option('-v, --verbose','enables verbose logging',false)
+		.argument('<input-filename>','the input filename')
+		.parse(process.argv);
 	return;
 };
 
