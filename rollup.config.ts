243c7cfa7388a5ade2f07bf418daedcab35c01e4
Wilson Foo Yu Kang <wilson.foo@customautosys.com>
2024-07-05 13:26:48 +0800

    Added rollup bundling

---
+++ rollup.config.ts
@@ -0,0 +1,27 @@
+import dts from 'rollup-plugin-dts';
+import esbuild from 'rollup-plugin-esbuild';
+import json from '@rollup/plugin-json';
+import nodeResolve from '@rollup/plugin-node-resolve';
+
+export default[{
+	input:'src/index.ts',
+	external:/node_modules/,
+	plugins:[
+		esbuild(),
+		json(),
+		nodeResolve()
+	],
+	output:[{
+		file:'dist/index.js',
+		format:'es',
+		sourcemap:true,
+		exports:'auto'
+	}]
+},{
+	input:'src/index.ts',
+	plugins:[dts()],
+	output:{
+		file:'dist/index.d.ts',
+		format:'es',
+	}
+}];
--- src/exports.ts
+++ src/exports.ts
@@ -1,21 +1,22 @@
 import cli from './functions/cli';
 
 import ArchiveFolderClauseRepository from './types/ArchiveFolderClauseRepository';
-import ArchiveFolder from './types/ArchiveFolder';
-import ClauseData from './types/ClauseData';
-import ClauseReference from './types/ClauseReference';
-import ClauseRepository from './types/ClauseRepository';
-import DocumentOutputReturn from './types/DocumentOutputReturn';
-import InputParameter from './types/InputParameter';
 import LegaldacClauseXmlScript from './types/LegaldacClauseXmlScript';
 import LegaldacDocument7Z from './types/LegaldacDocument7Z';
 import LegaldacDocumentXmlScript from './types/LegaldacDocumentXmlScript';
 import LegaldacXmlScript from './types/LegaldacXmlScript';
-import OutputDocumentTag from './types/OutputDocumentTag';
-import OutputReturn from './types/OutputReturn';
-import ParseOutputReturn from './types/ParseOutputReturn';
-import PreserveOrderXmlNode from './types/PreserveOrderXmlNode';
-import Statement from './types/Statement';
+
+import type ArchiveFolder from './types/ArchiveFolder';
+import type ClauseData from './types/ClauseData';
+import type ClauseReference from './types/ClauseReference';
+import type ClauseRepository from './types/ClauseRepository';
+import type DocumentOutputReturn from './types/DocumentOutputReturn';
+import type InputParameter from './types/InputParameter';
+import type OutputDocumentTag from './types/OutputDocumentTag';
+import type OutputReturn from './types/OutputReturn';
+import type ParseOutputReturn from './types/ParseOutputReturn';
+import type PreserveOrderXmlNode from './types/PreserveOrderXmlNode';
+import type Statement from './types/Statement';
 
 export{
 	cli,
